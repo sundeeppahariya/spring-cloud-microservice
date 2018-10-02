9e28944ffa80e471f32356b5a63a54706bc1481c
sundeeppahariya <pahariya.sundeep@gmail.com>
2018-10-02 19:45:42 +0530

    Adding sub modules

---
+++ spring-cloud-config-client/src/main/java/com/niksun/microservice/SpringCloudConfigClientApplication.java
@@ -0,0 +1,29 @@
+package com.niksun.microservice;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class SpringCloudConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigClientApplication.class, args);
+	}
+}
+
+@RefreshScope
+@RestController
+class MessageRestController {
+ 
+    @Value("${msg:Hello world - Config Server is not working..pelase check}")
+    private String msg;
+ 
+    @RequestMapping("/msg")
+    String getMsg() {
+        return this.msg;
+    }
+}
